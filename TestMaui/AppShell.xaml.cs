6f920a93fcba97f3a21c87db7b8f8e9da39c21b3
LAPTOP-SMKUHTUR\MEC <skyoffice0825@gmail.com>
2026-02-07 09:45:52 +0700

    刪除

--- TestMaui/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestMaui;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- TestMaui/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TestMaui")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TestMaui.Pages")]
