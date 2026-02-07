23d0e47991ea26f0a90086178ce19c9da766f318
LAPTOP-SMKUHTUR\MEC <skyoffice0825@gmail.com>
2026-02-07 09:24:08 +0700

    new project

---
+++ mobile/MIM.Inventory.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MIM.Inventory.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ mobile/MIM.Inventory.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MIM.Inventory.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
